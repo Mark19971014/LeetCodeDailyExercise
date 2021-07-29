57fc26b0db3f26206ff46b07195df2103497553e
Mark19971014 <72395579+Mark19971014@users.noreply.github.com>
2021-07-29 23:56:34 +0200

    Add files via upload

---
+++ Array/DuplicateNumber.java
@@ -0,0 +1,19 @@
+package Array;
+
+import java.util.Arrays;
+
+public class DuplicateNumber {
+	 public int duplicate(int[] nums) {
+		 int duplicate = -1;
+		 for (int i = 0; i <  nums.length; i++) {
+			 int cur = Math.abs(nums[i]);
+			 if(nums[cur] < 0) {
+				 duplicate = cur;
+				 break;
+			 }
+			 nums[cur] *= -1; 
+		 }
+		 
+		 return duplicate;
+	 }
+}
